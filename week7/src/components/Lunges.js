9d46d9107516ef4467da949a37fd869c379b9e64
mars-h-4639 <lhutchins@knights.ucf.edu>
2021-02-23 13:14:26 -0500

    Week 7 finish

---
+++ week7/src/components/Lunges.js
@@ -0,0 +1,15 @@
+import React, {useState} from 'react';
+
+function Sun () {
+    const [lunges, setlunges] = useState(0);
+
+    return (
+        <div>
+        <h2>Lunges: {lunges}</h2>
+        <button onClick={() => setlunges(lunges + 1)}>Lunge count</button>
+        <button onClick={() =>setlunges(0)}>Reset Counter</button>
+        </div>
+    )
+}
+
+export default Sun;
