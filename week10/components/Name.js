49d45841c00d9130f0d5e956d1c9e71a971eb322
mars-h-4639 <lhutchins@knights.ucf.edu>
2021-03-16 12:24:57 -0400

    Week10
    Week 10 finished

---
+++ week10/components/Name.js
@@ -0,0 +1,12 @@
+import React, {Component} from "React";
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class Name extends Component {
+    render() {
+        return (
+            <View>
+                <Text>Mars Hutchins</Text>
+            </View>
+        );
+    }
+}
